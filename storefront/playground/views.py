e4d1578f3ed7732786d9a690af6cac5167493a00
Tanya-Week <137870650+Tanya-Week@users.noreply.github.com>
2023-06-27 13:08:32 +0530

    Add files via upload

---
+++ storefront/playground/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+# request -> response
+# request render
+# action 
+
+def say_hello(request):
+    #The following things can be done in this function
+    #Pull data from a database
+    #Transform the data
+    #Send emails
+    return render(request,'hello.html',{'name':'Simran'})
